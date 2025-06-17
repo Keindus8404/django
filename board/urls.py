573ed7cb71e9a13763e6dd086f875884fa216eaf
keindus8404 <defrican@gmail.com>
2025-06-17 17:19:05 +0900

    8교시 마무리

--- board/urls.py
+++ board/urls.py
@@ -8,4 +8,8 @@ app_name = 'board'  # board 앱의 이름을 지정합니다. (선택 사항)
 urlpatterns = [
     path('', views.index, name = 'question_list'),  # board 앱의 index 뷰를 기본 URL로 설정
     path('<int:question_id>/', views.detail, name='question_detail'),  # 질문 상세 페이지
+    path('ask/create/<int:question_id>/',
+         views.create_answer, name='create_answer'),  # 답변 생성 URL
+    path('question/create/', views.create_question, name='create_question'),  # 질문 생성 URL
+    path('test/', views.test, name='test'),  # 테스트용 URL
 ]
--- board/views.py
+++ board/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Question  # Assuming you have a model named Question in models.py
-
+from .models import Question, Answer  # Assuming you have a model named Question in models.py
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    questions = Question.objects.order_by('-created_at')  # 모든 질문을 가져옵니다.
-    return render(request, 'board/index.html', {'questions': questions})
+    page_number = request.GET.get('page', 1)  # ← 기본값 1 지정 중요
+    questions = Question.objects.order_by('-created_at')
+    paginator = Paginator(questions, 10)
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'board/index.html', {
+        'page_obj': page_obj,
+        'questions': page_obj.object_list
+    })
 
 def detail(request, question_id):
     # question = Question.objects.get(id=question_id)  # 특정 질문을 가져옵니다.
     question = get_object_or_404(Question, id=question_id)  # 질문이 없으면 404 에러를 반환합니다.
     return render(request, 'board/detail.html', {'question': question})
+
+def create_answer(request, question_id):
+    question = get_object_or_404(Question, id = question_id)
+    Answer(question=question, content = request.POST['content']).save()  # 답변을 저장합니다.
+    return redirect('board:question_detail', question_id=question.id)  # 질문 상세 페이지로 리다이렉트
+
+def create_question(request):
+    if request.method == 'POST':
+        subject = request.POST.get('subject')
+        content = request.POST.get('content')
+        Question(subject=subject, content=content).save()        
+        return redirect('board:question_list')
+    return render(request, 'board/create_question.html')
+
+# 임시로 Question 객체를 100개 생성하는 뷰
+def test(request):
+    for i in range(100):
+        Question(subject=f'질문 {i+1}', content=f'질문 내용 {i+1}').save()
+    return HttpResponse('100개의 질문이 생성되었습니다.')
